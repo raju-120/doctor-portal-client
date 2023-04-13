f699cfeb88269f4defb9b05d176e39f59e79b6e4
Raju <ashfaqraju27@gmail.com>
2023-04-13 18:00:51 +0600

    testimonial Part added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,6 +4,7 @@ import InfoCards from '../InfoCards/InfoCards';
 import Services from '../Services/Services';
 import Exceptional from '../Exceptional/Exceptional';
 import MakeAppointment from '../MakeAppointment/MakeAppointment';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <Services></Services>
             <Exceptional></Exceptional>
             <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
         </div>
     );
 };
