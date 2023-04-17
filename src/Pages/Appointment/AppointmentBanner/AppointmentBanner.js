e5fb22b6a728839a316c06269daec5809fea7481
Raju <ashfaqraju27@gmail.com>
2023-04-17 15:53:43 +0600

    background img addded

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import chair from '../../..//assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
+import './AppointmentBanner.css';
 
 const AppointmentBanner = ({selectedDate,setSelectedDate}) => {
     
     return (
-        <header className='my-6'>
-            <div className="hero ">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src={chair} alt='dentist chair ' className="max-w-sm lg:w-1/2 rounded-lg shadow-2xl" />
-                    <div className='mr-6'>
-                        <DayPicker 
-                            mode='single'
-                            selected={selectedDate}
-                            onSelect={setSelectedDate}
-                        />
+        <section className='ApImg'>
+            <header className='my-6'>
+                <div className="hero ">
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                        <img src={chair} alt='dentist chair ' className="max-w-sm lg:w-1/2 rounded-lg shadow-2xl" />
+                        <div className='mr-6'>
+                            <DayPicker 
+                                mode='single'
+                                selected={selectedDate}
+                                onSelect={setSelectedDate}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </section>
     );
 };
 
